732208ea6c672d695e97cfa984f38cee26ebde33
Liam Tyron Pather <st10248479@vcconnect.edu.za>
2025-09-18 09:52:27 +0200

    Commit 1 - Splash, intro, login and reg

---
+++ ToastApplication/app/src/main/java/vcmsa/projects/toastapplication/LoginActivity.kt
@@ -0,0 +1,11 @@
+package vcmsa.projects.toastapplication
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login) // <-- this will be your login UI XML
+    }
+}
