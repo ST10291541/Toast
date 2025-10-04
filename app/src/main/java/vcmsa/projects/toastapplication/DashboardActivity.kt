e9f4dac2cc2856c06dee1001e359c24b32cf899d
Elisha Stephen <129074116+ST10291541@users.noreply.github.com>
2025-10-04 14:29:24 +0200

    Changes

--- app/src/main/java/vcmsa/projects/toastapplication/DashboardActivity.kt
+++ app/src/main/java/vcmsa/projects/toastapplication/DashboardActivity.kt
@@ -2,6 +2,8 @@ package vcmsa.projects.toastapplication
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
